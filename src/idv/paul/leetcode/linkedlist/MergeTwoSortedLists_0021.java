e6b5fcc2389dfce258b9935a2964ba07e6e103f7
Paul <jindotw@gmail.com>
2023-08-14 16:46:01 +0800

    feat: solved 21. MergeTwoSortedLists [easy]

--- src/idv/paul/leetcode/linkedlist/MergeTwoSortedLists_0021.java
+++ src/idv/paul/leetcode/linkedlist/MergeTwoSortedLists_0021.java
@@ -24,7 +24,51 @@ Output: [0]
 
 import idv.paul.leetcode.util.ListNode;
 
-public class MergeTwoSortedLists {
+public class MergeTwoSortedLists_0021 {
+
+    public static ListNode mergeTwoLists3(ListNode list1, ListNode list2) {
+        if (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                list1.next = mergeTwoLists3(list1.next, list2);
+                return list1;
+            } else {
+                list2.next = mergeTwoLists3(list1, list2.next);
+                return list2;
+            }
+        }
+
+        return list1 != null ? list1 : list2;
+    }
+
+    public static ListNode mergeTwoLists2(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0, null);
+        ListNode start = dummy;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                start.next = list1;
+                list1 = list1.next;
+            } else {
+                start.next = list2;
+                list2 = list2.next;
+            }
+            start = start.next;
+        }
+
+        while (list1 != null) {
+            start.next = list1;
+            list1 = list1.next;
+            start = start.next;
+        }
+
+        while (list2 != null) {
+            start.next = list2;
+            list2 = list2.next;
+            start = start.next;
+        }
+
+        return dummy.next;
+    }
+
     public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         // dummy node
         ListNode list = new ListNode(-1);
@@ -59,8 +103,8 @@ public class MergeTwoSortedLists {
 
     public static void main(String... argv) {
         ListNode l1 = ListNode.constructLinkedListNode(new int[]{1,7,9});
-        ListNode l2 = ListNode.constructLinkedListNode(new int[]{11,13,19});
-        ListNode l3 = mergeTwoLists(l1, l2);
+        ListNode l2 = ListNode.constructLinkedListNode(new int[]{1,4,8, 20});
+        ListNode l3 = mergeTwoLists3(l1, l2);
         ListNode.printListNode(l3);
     }
 }
