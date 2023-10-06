c5ede8717f37c63542b011b50d0b4f39bbcd6e19
Paul <jindotw@gmail.com>
2023-10-06 09:19:03 +0800

    feat: solved 55. Jump Game [medium]

---
+++ src/idv/paul/leetcode/greedy/JumpGame_0055.java
@@ -0,0 +1,53 @@
+package idv.paul.leetcode.greedy;
+
+/*
+https://leetcode.com/problems/jump-game/
+
+55. Jump Game
+Solved
+Medium
+Topics
+Companies
+You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+Return true if you can reach the last index, or false otherwise.
+
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+Example 2:
+
+Input: nums = [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+
+
+Constraints:
+
+1 <= nums.length <= 104
+0 <= nums[i] <= 105
+ */
+
+public class JumpGame_0055 {
+	public static boolean canJump(int[] nums) {
+		if (nums.length == 1) {
+			return true;
+		}
+		int maxReach = 0;
+		for (int i=0; i<=maxReach; i++) {
+			maxReach = Math.max(maxReach, i + nums[i]);
+			if (maxReach >= nums.length-1) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static void main(String ...argv) {
+		int[] nums = new int[]{3, 2, 1, 0, 4};
+		// int[] nums = new int[]{2, 0};
+		System.out.println(canJump(nums));
+	}
+}
